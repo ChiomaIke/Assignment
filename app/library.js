61af913090b501bbfeef6b2e6bb82d2d1dbfa453
Chioma Ike <nwigwechioma@gmail.com>
2016-11-25 11:58:42 +0000

    tested ok

--- app/library.js
+++ app/library.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-	arithGeo:function(arr){
-		var ap, gp;
+	aritGeo:function(arr){
+		/*var ap, gp;
 		for (i = 0; i < (arr.length - 2); i++) 
 		 if (!(ap = arr[i + 1] - arr[i] == arr[i + 2] - arr[i + 1]))
 		break;
@@ -14,7 +14,46 @@ module.exports = {
 
 		 if (arr.length == 0) return 0;
 
-		 return -1;
+		 return -1;*/
+		 
+		var ap;
+		var gp;
+		
+		for(i=0; i < (arr.length - 2); i++)
+		{
+			if ((arr[i+1] - arr[i]) != (arr[i+2] - arr[i+1]))
+			{
+				ap = false;
+				break;
+			}
+			ap = true;
+		}
+		
+		for(i=0; i < (arr.length - 2); i++)
+		{
+			if ((arr[i+1] / arr[i]) != (arr[i+2] / arr[i+1]))
+			{
+				gp = false;
+				break;
+			}
+			gp=true;
+		}
+		
+		if (arr.length == 0)
+		{
+			return 0;
+		}
+		if (gp == true)
+		{
+			return 'Geometric';
+		}
+		if (ap == true)		
+		{
+			return 'Arithmetic';
+		}
+		
+		return -1;
+		
 	},
 	
 	findMinMax:function(arr)
